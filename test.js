4d0fbed8b32c99fb7e2c8c8c4779741d9da3b0c3
RanaMayank <mayankchauhan8449383386@gmail.com>
2024-03-07 18:59:15 +0530

    added new file

---
+++ test.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "coder@google.com"
+var  accountpassword = "12345"
+accountCity = "Lucknow"
+
+accountEmail = "mc@Ch.com"
+accountpassword = "122133212"
+accountCity = "delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountpassword, accountCity])
